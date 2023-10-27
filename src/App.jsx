2a82a0c3bf5972a6ff64a5a9f084b1a5fc822fc5
Thuku7 <robethuku11@gmail.com>
2023-10-27 10:15:25 +0300

    Will add the navbar componet and maincontent to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './Components/NavBar'
+import MainContent from './Components/MainContent'
 
 function App() {
+  <div>
+    <NavBar />
+    <MainContent />
+  </div>
  
 }
 
